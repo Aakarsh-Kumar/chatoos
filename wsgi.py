ede0a2c95fa48873ce3e4dee888ba8c6f92a1eb2
Aakarsh-Kumar <aakarsh2504@gmail.com>
2021-09-25 22:45:17 +0530

    view room UI

--- wsgi.py
+++ wsgi.py
@@ -167,5 +167,5 @@ def error_404(e):
     return "404 Not Found"
 
 if __name__ == '__main__':
-    # socketio.run(app,debug=True)
+    # socketio.run(app,debug=True, host="192.168.1.8", port=1111)
     app.run()
