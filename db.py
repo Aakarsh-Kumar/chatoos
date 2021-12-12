c862aa617010f428961a8b90634c6a1ae1ba33af
Aakarsh-Kumar <aakarsh2504@gmail.com>
2021-12-12 23:39:10 +0530

    noti

--- db.py
+++ db.py
@@ -24,7 +24,7 @@ def save_user(username,name,dp_url):
     users_collection.insert_one({'_id':username,'name':name,'dp_url':dp_url,'notification-status':None,'notification-token':None})
 
 def update_user_notification_status(username,status,token):
-    users_collection.update_one({'_id':username},{'$set':{'notification-status':status,'notification-token':token}})
+    return users_collection.update_one({'_id':username},{'$set':{'notification-status':status,'notification-token':token}})
 
 def get_user(username):
     user_data = users_collection.find_one({'_id':username})
